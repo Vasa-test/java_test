601fafbfa469d480792b942f5fd7e6aa76f7f712
ASA <ilysikov8+github_resident@gmail.com>
2020-10-12 11:36:00 +0300

    Измененны настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/my.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/my.java
@@ -1,8 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class my {
-
- public static void main(String[] args){
-System.out.println("Hello, world");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
 }
