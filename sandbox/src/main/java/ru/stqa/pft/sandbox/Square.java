69562e65904b3647d49c6974026d02d1dcc6f0fa
ASA <ilysikov8+github_resident@gmail.com>
2020-10-14 18:26:53 +0300

    geckodriver и первый тест в фаерфоксе
    С помощью дополнения к браузеру Selenium IDE

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+  public int l;
+
+  public Square(int l) {
+    this.l = l;
+  }
+  public int area() {
+    return this.l * this.l;
+  }
+}
