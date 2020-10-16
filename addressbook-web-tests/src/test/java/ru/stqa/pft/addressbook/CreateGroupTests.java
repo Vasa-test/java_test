8eebd2e79f0e09513299cc1883a8fef462e860df
ASA <ilysikov8+github_resident@gmail.com>
2020-10-16 18:44:46 +0300

    Провели рефакторинг
    Посоздавали новые методы для самого теста. Создали новый класс для ввода данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateGroupTests.java
@@ -12,32 +12,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Test;
-import org.junit.Before;
-import org.junit.After;
-
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
-
-import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class CreateGroupTests {
   private WebDriver driver;
@@ -51,12 +27,16 @@ public class CreateGroupTests {
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
     driver.get("http://localhost/addressbook/index.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     driver.manage().window().setSize(new Dimension(1440, 833));
     driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("user")).sendKeys(username);
     driver.findElement(By.id("LoginForm")).click();
     driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.name("pass")).sendKeys(password);
     driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     driver.findElement(By.cssSelector("body")).click();
   }
@@ -68,15 +48,36 @@ public class CreateGroupTests {
 
   @Test
   public void testCreateGroup() {
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("test6");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("test6");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("test6");
-    driver.findElement(By.name("submit")).click();
+    gotoGroup();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test6", "test6", "test6"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
     driver.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroup() {
+    driver.findElement(By.linkText("groups")).click();
   }
 }
