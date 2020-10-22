2e512943f13c8683c12605a948e07db9a091349a
ASA <ilysikov8+github_resident@gmail.com>
2020-10-22 11:21:48 +0300

    Создали базовый класс для классов помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -1,25 +1,22 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-  private WebDriver driver;
+public class SessionHelper extends HelperBase {
 
   public SessionHelper(WebDriver driver) {
 
-    this.driver = driver;
+    super(driver);
   }
 
   public void login(String username, String password) {
-    driver.manage().window().setSize(new Dimension(1440, 833));
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    driver.findElement(By.cssSelector("body")).click();
+    setSizeWindow();
+    type(By.name("user"),username);
+    click(By.id("LoginForm"));
+    type(By.id("LoginForm"),password);
+    click(By.cssSelector("input:nth-child(7)"));
+    click(By.cssSelector("body"));
   }
+
 }
