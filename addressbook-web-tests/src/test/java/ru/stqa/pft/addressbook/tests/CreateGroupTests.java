d61a1025e6de1b75958aa81dcd02cd76e57d19f6
ASA <ilysikov8+github_resident@gmail.com>
2020-11-05 11:27:39 +0200

    Реализовна проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTests.java
@@ -8,10 +8,7 @@ public class CreateGroupTests extends TestBase {
   @Test
   public void testCreateGroup() {
     app.getNavigationHelper().gotoGroup();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test6", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test6", null, null));
   }
 
 }
