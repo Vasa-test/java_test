6c2ccd04a5e4af5c039e24225fc58cd9ae2e2290
ASA <ilysikov8+github_resident@gmail.com>
2020-11-09 11:58:27 +0200

    Написали проверку кол-ва элементов после любых изменений
    Список всех элементов для подсчета кол-ва поместили в колекцию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,7 +9,10 @@ public class CreateGroupTests extends TestBase {
   @Test
   public void testCreateGroup() {
     app.getNavigationHelper().gotoGroup();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test6", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before +1);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification (){
     app.getNavigationHelper().gotoGroup();
+    int before = app.getGroupHelper().getGroupCount();
     if(! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test6", null, null));
     }
@@ -16,5 +18,7 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("test6", "test6", "test6"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before);
   }
 }
