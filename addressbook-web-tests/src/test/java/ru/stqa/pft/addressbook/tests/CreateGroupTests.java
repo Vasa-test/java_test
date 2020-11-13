bc40bfcbc9d9c9375ddec0b4ac3a62685ccf85c0
ASA <ilysikov8+github_resident@gmail.com>
2020-11-13 17:42:50 +0200

    Сверяем объекты во время создания новой группы
    Использовали цикл что бы найти id и добавить его в before

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTests.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class CreateGroupTests extends TestBase {
@@ -12,9 +13,20 @@ public class CreateGroupTests extends TestBase {
   public void testCreateGroup() {
     app.getNavigationHelper().gotoGroup();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test6", null, null));
+    GroupData group = new GroupData("test6", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size() +1);
+
+    before.add(group);
+    int max = 0;
+    for(GroupData g : after){
+      if(g.getId() > max){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
