35f336fe2a1e3bb9496cba9900ab000771eff670
ASA <ilysikov8+github_resident@gmail.com>
2021-02-04 14:18:52 +0200

    Добавили новый тест HbConnectionTest
    Где для работы с базой данных использовали другую библиотеку, а именно hibernate-core

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,19 +3,36 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
   @XStreamOmitField
-  private  int id;
+  @Id
+  @Column(name = "group_id")
+  private  int id = Integer.MAX_VALUE;
   @Expose
-  private final String name;
+  @Column(name = "group_name")
+  private String name;
   @Expose
-  private final String header;
+  @Column(name = "group_header")
+  @Type(type = "text")
+  private String header;
   @Expose
-  private final String footer;
+  @Column(name = "group_footer")
+  @Type(type = "text")
+  private String footer;
 
+  //конструктор по умолчанию
+  public GroupData(){
+  }
   public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
@@ -23,11 +40,11 @@ public class GroupData {
     this.footer = footer;
   }
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
+
   public int getId() {
     return id;
   }
